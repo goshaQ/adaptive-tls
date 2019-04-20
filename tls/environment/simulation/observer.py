Fix calculation of throughput

tls/environment/simulation/observer.py
@@ -205,7 +205,7 @@ class Observer:
         :return: none.
         """
 
-        self.current_observation = np.zeros((c.MESH_SIZE, c.MESH_SIZE))
+        self.current_observation = np.zeros((c.MESH_SIZE, c.MESH_SIZE, 1))
 
         for junction, relative_offset in self.topology:
             polygon = self._get_junction_polygon(junction, relative_offset)
@@ -231,7 +231,7 @@ class Observer:
                             if not np.array_equal(idx, self._clamp(idx, min=0, max=c.MESH_SIZE - 1)):
                                 continue
 
-                            self.current_observation[tuple(idx)] = 1
+                            self.current_observation[tuple(idx)][0] = 1
                     cursor += step
         return self.current_observation
 

tls/environment/sumo.py
@@ -60,10 +60,11 @@ class SUMOEnv(MultiAgentEnv):
             sumo_binary = sumolib.checkBinary('sumo-gui')
         else:
             sumo_binary = sumolib.checkBinary('sumo')
-        self._sumo_cmd = [sumo_binary, '--start', '--quit-on-end', '-c', config_file]
+        self._sumo_cmd = [sumo_binary, '--start', '--quit-on-end', '--threads', '4', '-c', config_file]
 
+        # TODO: Revise
         self.observation_space = spaces.Box(low=0.0, high=1.0,
-                                            shape=Collaborator.get_observation_space_shape(), dtype=np.float32)
+                                            shape=Collaborator.get_observation_space_shape(), dtype=np.float32),
         self.action_space = spaces.Discrete(Collaborator.get_action_space_shape())
 
     def step(self, actions):
@@ -91,6 +92,7 @@ class SUMOEnv(MultiAgentEnv):
         Returns:
             observation (object): The initial observation for the new episode after reset.
         """
+        self.close()
         traci.start(self._sumo_cmd)
 
         # Reinitialize the collaborator since the connection changed
@@ -99,4 +101,3 @@ class SUMOEnv(MultiAgentEnv):
 
     def close(self):
         self.collaborator = None
-        traci.close()